89c7453c3d2c239eaef35e268215f58cde1043f6
DavidWells <hello@davidwells.io>
2020-02-05 15:28:02 -0800

    update plugin
    Signed-off-by: DavidWells <hello@davidwells.io>

--- plugins/npm-stats/index.js
+++ plugins/npm-stats/index.js
@@ -4,15 +4,17 @@ const { promisify } = require('util')
 const npmtotal = require('npmtotal')
 const writeFile = promisify(fs.writeFile)
 const readFile = promisify(fs.readFile)
-const NAME = 'netlify-plugin-npm-stats-page'
+
+const NAME = 'netlify-plugin-my-npm-stats'
 
 module.exports = (conf) => {
   return {
     name: NAME,
     async onBuild({ netlifyConfig, utils }) {
       const { cache } = utils
-      const npmStatsDataPath = `${netlifyConfig.build.publish}/npm-stats.json`
-      if (!conf.npmUserName) {
+      const { npmUserName, excludePackages } = conf
+      const npmStatsDataPath = getDataFilePath(netlifyConfig.build.publish, npmUserName)
+      if (!npmUserName) {
         throw new Error(`Missing npmUserName in ${NAME}`)
       }
 
@@ -20,27 +22,27 @@ module.exports = (conf) => {
       if (!hasFileCached) {
         console.log('No cache found. Fetch data from NPM')
         let options = {}
-        if (conf.exclude) {
-          options.exclude = conf.exclude
+        if (excludePackages) {
+          options.exclude = excludePackages
         }
-        console.log(`Fetching NPM data for ${conf.npmUserName}`)
-        console.log(`This can take a while depending on the number of packages ${conf.npmUserName} has`)
-        const stats = await npmtotal(conf.npmUserName, options)
-        console.log('stats', stats)
+        console.log(`Fetching NPM data for ${npmUserName}`)
+        console.log(`This can take a while depending on the number of packages ${npmUserName} has`)
+        const stats = await npmtotal(npmUserName, options)
         // Write file
         await writeFile(npmStatsDataPath, JSON.stringify(stats, null, 2))
         // Then save to cache for later
         await cache.save([ npmStatsDataPath ], { ttl: 3600 })
         return
       }
-
-      console.log(`Found cache`)
+      // Has cache, go fast
+      console.log(`Found NPM stats cache`)
       console.log(`Restore cache to ${npmStatsDataPath}`)
       await cache.restore([ npmStatsDataPath ])
       console.log('Loaded a previous cache. ⚡️')
     },
-    async onPostBuild({ netlifyConfig, utils }) {
-      const npmStatsDataPath = `${netlifyConfig.build.publish}/npm-stats.json`
+    async onPostBuild({ netlifyConfig, constants, utils }) {
+      const { npmUserName } = conf
+      const npmStatsDataPath = getDataFilePath(netlifyConfig.build.publish, npmUserName)
 
       const contents = await readFile(npmStatsDataPath, 'utf-8')
       const data = JSON.parse(contents)
@@ -55,20 +57,29 @@ module.exports = (conf) => {
   }
 }
 
+function getDataFilePath(buildDir, username) {
+  return `${buildDir}/npm-stats-${username}.json`
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
 /* Utility function for rendering HTML */
 function generateHtmlPage(username, data) {
   const list = data.stats.map((pkg) => {
     const [name, count] = pkg
-    return `<li>${name} - ${count}</li>`
+    return `<li>${name} - ${numberWithCommas(count)}</li>`
   })
   return `
     <html>
+      <title>${username} NPM stats</title>
       <style>
         h1 { color: #73757d }
       </style>
       <body>
         <h1>${username} NPM Stats</h1>
-        <h3>Total downloads: ${data.sum}</h3>
+        <h3>Total downloads: ${numberWithCommas(data.sum)}</h3>
         <ul>
           ${list.join('\n')}
         </ul>
